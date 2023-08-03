e3e72f6800545dc5056a7ad193b7522ce23fa6bc
unknown <lipeamaralsantos@gmail.com>
2023-08-03 18:33:01 -0300

    code update

--- README.md
+++
@@ -1,2 +0,0 @@
-# AI_from_scratch
-My first AI, I made it from scratch to learn the basics of machine learning and MLP models
--- main.py
+++ main.py
@@ -5,24 +5,37 @@ from neural_network import NeuralNetwork
 from fake_fruits import FruitsData, FakeFruit
 from activation_module import ActivationFunctions
 
+n_epochs = 1000
+layer_shape = [(5, 25), (25, 25)]
+
+
 score = 0
-n_epochs = int(input('n_epochs: '))
-layer_shape = [(3, 25), (25, 25), (25, 25)]
-x_coord = []
-y_coord = []
+correct_predictions = 0
+one_hundred_epochs = 0
+accuracy = 0
+
+epochs_x_coord = []
+one_hundred_epochs_x_coord = []
+score_y_coord = []
+accuracy_y_coord = []
+
 
 model = NeuralNetwork()
 
 model.create(
     net_shape=layer_shape,
-    activators=[ActivationFunctions.softmax, ActivationFunctions.softmax, ActivationFunctions.softmax]
+    activators=[ActivationFunctions.softmax, ActivationFunctions.softmax]
     )
 
 for epoch in range(1, n_epochs + 1):
 
     fruit = FakeFruit()
     inputs = np.array(
-        object=[fruit.diameter, fruit.weight, fruit.texture],
+        object=[fruit.diameter,
+                fruit.weight,
+                fruit.texture,
+                fruit.sugar_level,
+                fruit.ph_level],
         dtype=np.float64
     )
 
@@ -32,11 +45,19 @@ for epoch in range(1, n_epochs + 1):
 
     if fruit.name == predicted_fruit:
         score += 1
+        correct_predictions += 1
     else:
         score -= 1
 
-    x_coord.append(epoch)
-    y_coord.append(score)
+    if epoch % 100 == 1:
+        one_hundred_epochs += 1
+        accuracy = (correct_predictions / epoch) * 100 
+        correct_predictions = 0
+        
+    epochs_x_coord.append(epoch)
+    one_hundred_epochs_x_coord.append(one_hundred_epochs)
+    score_y_coord.append(score)
+    accuracy_y_coord.append(accuracy)        
     
     if epoch / 100 == 1:
         model.save_data()
@@ -44,15 +65,17 @@ for epoch in range(1, n_epochs + 1):
     os.system('cls')
     print('-'*100, 
           f'\nScore: {score}',
+          f'\nAccuracy: {accuracy:.3f}',
           f'\nProgress: {epoch / n_epochs * 100:.3f}%',
           f'\nRemaining epochs: {n_epochs - epoch}'
     )
 
 model.save_data()
+
 plt.plot(
-    x_coord,
-    y_coord,
-    color='Red'
+    one_hundred_epochs_x_coord,
+    accuracy_y_coord,
+    color='Blue'
 )
 plt.title(f'Using Softmax\n{layer_shape}')
 plt.xlabel('Epoch')
